2c52da4ce99926800ebc4d045f92971908434903
Job Alvarez <jobitoalv@gmail.com>
2023-01-12 16:01:45 +1000

    setting up appProvide

--- src/context.js
+++ src/context.js
@@ -1,2 +1,12 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+
+const AppContex = React.createContext();
+
+const AppProvider = ({ children }) => {
+    const [ isSidebarOpen, setIsSidebarOpen ] = useState(true);
+    return <AppContex.Provider>
+        {children}
+    </AppContex.Provider>
+}
