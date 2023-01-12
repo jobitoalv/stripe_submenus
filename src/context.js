14ce5f649c31d3ac8f105587809453315d0d1c70
Job Alvarez <jobitoalv@gmail.com>
2023-01-12 16:08:22 +1000

    setting const for close and open sidebar and modal

--- src/context.js
+++ src/context.js
@@ -8,6 +8,23 @@ const AppProvider = ({ children }) => {
     const [ isSidebarOpen, setIsSidebarOpen ] = useState(true);
     const [ isSModalOpen, setIsModalOpen ] = useState(true);
 
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    }
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    }
+
+    const openModal = () => {
+        setIsModalOpen(true)
+    }
+
+    const closeModal = () => {
+        setIsModalrOpen(false)
+    }
+
     return <AppContex.Provider>
         {children}
     </AppContex.Provider>
