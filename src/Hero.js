01ad9c38d2d3ceb6784a5808d0840efbce057df9
Job Alvarez <jobitoalv@gmail.com>
2023-01-13 10:05:55 +1000

    UI page completed

--- src/Hero.js
+++ src/Hero.js
@@ -9,6 +9,13 @@ const Hero = () => {
     <div className='hero-center'>
       <article className='hero-info'>
         <h1>Payment Infrastructure for the internet</h1>
+        <p>Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.
+        </p>
+        <button className='btn'>
+          start now
+        </button>
       </article>
       <article className='hero-images'>
         <img src={phoneImg} className= "phone-img" alt="phone"/>
