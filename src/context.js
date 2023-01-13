de46309db8fc3fa5b33db2582a3f671625adc64d
Job Alvarez <jobitoalv@gmail.com>
2023-01-13 10:51:30 +1000

    hide side bar

--- src/context.js
+++ src/context.js
@@ -5,7 +5,7 @@ import sublinks from './data'
 const AppContex = React.createContext();
 
 export const AppProvider = ({ children }) => {
-    const [ isSidebarOpen, setIsSidebarOpen ] = useState(true);
+    const [ isSidebarOpen, setIsSidebarOpen ] = useState(false);
     const [ isSubmenuOpen, setIsSubmenuOpen ] = useState(true);
 
 
