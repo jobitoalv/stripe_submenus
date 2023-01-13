cbf9abd5fe8c995f53952c9b4777cf814eb8cb85
Job Alvarez <jobitoalv@gmail.com>
2023-01-13 11:10:52 +1000

    complete page set up

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,29 +1,44 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
-import { useGlobalContext } from './context'
+import React from 'react';
+import logo from './images/logo.svg';
+import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from './context';
 
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  const displaySubmenu = (e) => {
+    const page = e.target.textContent;
+    const tempBtn = e.target.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+    openSubmenu(page, { center, bottom });
+  };
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains('link-btn')) {
+      closeSubmenu();
+    }
+  };
   return (
-    <nav className='nav'>
+    <nav className='nav' onMouseOver={handleSubmenu}>
       <div className='nav-center'>
         <div className='nav-header'>
-          <img src={logo} className='nav-log' alt="stripe" />
+          <img src={logo} className='nav-logo' alt='' />
           <button className='btn toggle-btn' onClick={openSidebar}>
-            <FaBars/>
+            <FaBars />
           </button>
         </div>
-      
         <ul className='nav-links'>
           <li>
-            <button className='link-btn'>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
               products
             </button>
-            <button className='link-btn'>
+          </li>
+          <li>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
               developers
             </button>
-            <button className='link-btn'>
+          </li>
+          <li>
+            <button className='link-btn' onMouseOver={displaySubmenu}>
               company
             </button>
           </li>
@@ -31,7 +46,7 @@ const Navbar = () => {
         <button className='btn signin-btn'>Sign in</button>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/context.js
+++ src/context.js
@@ -1,46 +1,48 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+const AppContext = React.createContext();
 
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [page, setPage] = useState({ page: '', links: [] });
+  const [location, setLocation] = useState({});
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
 
-const AppContex = React.createContext();
-
-export const AppProvider = ({ children }) => {
-    const [ isSidebarOpen, setIsSidebarOpen ] = useState(false );
-    const [ isSubmenuOpen, setIsSubmenuOpen ] = useState(true);
-
-
-    const openSidebar = () => {
-        setIsSidebarOpen(true)
-    }
-
-    const closeSidebar = () => {
-        setIsSidebarOpen(false)
-    }
-
-    const openSubmenu = () => {
-        setIsSubmenuOpen(true)
-    }
-
-    const closeSubmenu = () => {
-        setIsSubmenuOpen(false)
-    }
-
-    return(
-    <AppContex.Provider 
-     value={{
-        isSubmenuOpen, 
-        isSidebarOpen, 
-        openSubmenu, 
-        openSidebar, 
-        closeSubmenu, 
-        closeSidebar
-    }}
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+        page,
+        location,
+      }}
     >
-        {children}
-    </AppContex.Provider>
-    )
-}
-
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
 export const useGlobalContext = () => {
-    return useContext(AppContex)
-}
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
